20dc3bc992025f17f8220c4269808c2632b2abf9
eafernandezm01 <eafernandezm01@educarex.es>
2024-01-04 16:27:05 +0100

    ,

--- js/script.js
+++ js/script.js
@@ -4,19 +4,19 @@ function ocultaResultado ({target}=e){
     if (target.tagName !== 'DIV'){
         target = target.parentElement;
     }
-    target.setAttribute ('class','resultado-hidden');
+    target?.setAttribute ('class','resultado-hidden');
 }
 
 function muestraResultado({target}=e){
    const parentElement=target.parentElement;
    const resultado=parentElement.querySelector ('.resultado-hidden');
-   resultado.setAttribute ('class','resultado-show');
-   resultado.addEventListener ('click', ocultaResultado);
+   resultado?.setAttribute ('class','resultado-show');
+   resultado?.addEventListener ('click', ocultaResultado);
 }
 
 function inicializar (){
     const nodeList = document.querySelectorAll('.ejemplo>code');
     nodeList.forEach(node => {
-        node.addEventListener ('click', muestraResultado);
+        node?.addEventListener ('click', muestraResultado);
     });
 }
